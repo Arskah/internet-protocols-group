924064158db45247a7d03ff95f398ee102c4ea1d
Aarni Halinen <aarni.halinen@aalto.fi>
2019-03-18 01:04:15 +0200

    Automate joining

--- backend2/public/js/app.js
+++ backend2/public/js/app.js
@@ -1,12 +1,4 @@
 window.addEventListener('load', () => {
-  // Chat platform
-  const chatTemplate = Handlebars.compile($('#chat-template').html());
-  const chatContentTemplate = Handlebars.compile($('#chat-content-template').html());
-  const chatEl = $('#chat');
-  const formEl = $('.form');
-  const messages = [];
-  let username;
-
   // Local Video
   const localImageEl = $('#local-image');
   const localVideoEl = $('#local-video');
@@ -18,14 +10,6 @@ window.addEventListener('load', () => {
 
   // Hide cameras until they are initialized
   localVideoEl.hide();
-
-  // Add validation rules to Create/Join Room Form
-  formEl.form({
-    fields: {
-      roomName: 'empty',
-      username: 'empty',
-    },
-  });
 
   // create our webrtc connection
   const webrtc = new SimpleWebRTC({
@@ -61,16 +45,6 @@ window.addEventListener('load', () => {
     remoteVideosCount += 1;
   });
 
-  // Update Chat Messages
-  const updateChatMessages = () => {
-    const html = chatContentTemplate({ messages });
-    const chatContentEl = $('#chat-content');
-    chatContentEl.html(html);
-    // automatically scroll downwards
-    const scrollHeight = chatContentEl.prop('scrollHeight');
-    chatContentEl.animate({ scrollTop: scrollHeight }, 'slow');
-  };
-
   // Post Local Message
   const postMessage = (message) => {
     const chatMessage = {
@@ -80,42 +54,6 @@ window.addEventListener('load', () => {
     };
     // Send to all peers
     webrtc.sendToAll('chat', chatMessage);
-    // Update messages locally
-    messages.push(chatMessage);
-    $('#post-message').val('');
-    updateChatMessages();
-  };
-
-  // Display Chat Interface
-  const showChatRoom = (room) => {
-    formEl.hide();
-    const html = chatTemplate({ room });
-    chatEl.html(html);
-    const postForm = $('form');
-    postForm.form({
-      message: 'empty',
-    });
-    $('#post-btn').on('click', () => {
-      const message = $('#post-message').val();
-      postMessage(message);
-    });
-    $('#post-message').on('keyup', (event) => {
-      if (event.keyCode === 13) {
-        const message = $('#post-message').val();
-        postMessage(message);
-      }
-    });
-  };
-
-  // Register new Chat Room
-  const createRoom = (roomName) => {
-    // eslint-disable-next-line no-console
-    console.info(`Creating new room: ${roomName}`);
-    webrtc.createRoom(roomName, (err, name) => {
-      formEl.form('clear');
-      showChatRoom(name);
-      postMessage(`${username} created chatroom`);
-    });
   };
 
   // Join existing Chat Room
@@ -123,7 +61,6 @@ window.addEventListener('load', () => {
     // eslint-disable-next-line no-console
     console.log(`Joining Room: ${roomName}`);
     webrtc.joinRoom(roomName);
-    showChatRoom(roomName);
     postMessage(`${username} joined chatroom`);
   };
 
@@ -131,23 +68,14 @@ window.addEventListener('load', () => {
   webrtc.connection.on('message', (data) => {
     if (data.type === 'chat') {
       const message = data.payload;
-      messages.push(message);
-      updateChatMessages();
+      console.log(message);
     }
   });
 
-  // Room Submit Button Handler
-  $('.submit').on('click', (event) => {
-    if (!formEl.form('is valid')) {
-      return false;
-    }
-    username = $('#username').val();
-    const roomName = $('#roomName').val().toLowerCase();
-    if (event.target.id === 'create-btn') {
-      createRoom(roomName);
-    } else {
-      joinRoom(roomName);
-    }
+  $((event) => {
+    username = 'testuser' + remoteVideosCount;
+    const roomName = 'test';
+    joinRoom(roomName);
     return false;
   });
 });
